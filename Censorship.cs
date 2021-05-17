849533a630892bfa519af75cf866c5485aa71217
charlesteets <71031886+charlesteets@users.noreply.github.com>
2021-05-17 09:16:15 -0400

    Add files via upload

---
+++ Censorship.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EradicateFreeSpeech
+{
+    public static class Censorship
+    {
+        /// <summary>
+        /// A string extension method that determines whether speech should be censored.
+        /// </summary>
+        /// <value>
+        /// The text to be analyzed and either censored or permitted.
+        /// </value>
+        /// <returns>
+        /// true if the input should be censored, false if the input is permitted
+        /// </returns>
+        public static bool ShouldBeCensored(this string _)
+        {
+            return false;
+        }
+    }
+}
